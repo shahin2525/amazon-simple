77aa3bfc2388116fb805376007df88c395f57d9e
shahin2525 <shahin451999@gmail.com>
2023-04-30 21:14:49 -0700

    amajon compilited

--- src/comonnets/Cart/Cart.jsx
+++ src/comonnets/Cart/Cart.jsx
@@ -4,9 +4,14 @@ const Cart = ({ cart }) => {
   //   const { cart } = props;
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
 
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    // if (product.quantity === 0) {
+    //   product.quantity = 1;
+    // }
+    quantity = quantity + product.quantity;
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
   }
   const tax = (totalPrice * 7) / 100;
@@ -15,7 +20,7 @@ const Cart = ({ cart }) => {
     <div className="cart">
       <h2>Order Summary</h2>
       <div className="cart-info">
-        <p>Selected Items: {cart.length}</p>
+        <p>Selected Items: {quantity}</p>
         <p>Total-Price: ${totalPrice}</p>
         <p>Total-Shipping-Charge: ${totalShipping}</p>
         <p>Tax: ${tax.toFixed(2)}</p>
--- src/comonnets/Shop/shop.jsx
+++ src/comonnets/Shop/shop.jsx
@@ -32,7 +32,17 @@ const Shop = () => {
 
   const addToCart = (product) => {
     // console.log(product);
-    const newCart = [...cart, product];
+    let newCart = [];
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id == !product.id);
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
